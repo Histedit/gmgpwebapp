b2a68e2cfd28ed5a0857d9fe3326f1d1ce99c422
Hong-il, Yang <histedit@gmail.com>
2018-08-15 19:13:01 +0900

    롤백......

--- js/onclick.js
+++ js/onclick.js
@@ -1,7 +1,10 @@
 
-document.getElementsByClassName('headerMenu')[0].onclick = function(){ window.location.href = "home.html"};
-document.getElementsByClassName('headerMenu')[1].onclick = function(){ window.location.href = "notice.html"};
-document.getElementsByClassName('headerMenu')[2].onclick = function(){ window.location.href = "calendar.html"};
-document.getElementsByClassName('headerMenu')[3].onclick = function(){ window.location.href = "report.html"};
-document.getElementsByClassName('headerMenu')[4].onclick = function(){ window.location.href = "member.html"};
+function clickFunction(){
+	document.getElementsByClassName('headerMenu')[0].onclick = function(){ window.location.href = "./home.html" };
+	document.getElementsByClassName('headerMenu')[1].onclick = function(){ window.location.href = "./notice.html" };
+	document.getElementsByClassName('headerMenu')[2].onclick = function(){ window.location.href = "./calendar.html" };
+	document.getElementsByClassName('headerMenu')[3].onclick = function(){ window.location.href = "./report.html" };
+	document.getElementsByClassName('headerMenu')[4].onclick = function(){ window.location.href = "./member.html" };
+}
 
+clickFunction();
